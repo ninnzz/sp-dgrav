000b314c4dc9b908c616750c9b5634f3bff62399
ninz <nreclarin@gmail.com>
2013-03-18 16:50:02 -0800

    with viewing nung mga data gamit ung google chart

--- assets/js/public.js
+++ assets/js/public.js
@@ -1,7 +1,44 @@
-function preLoad(){
-	
+google.load('visualization', '1', {packages: ['motionchart']});
 
+    function drawVisualization() {
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Application');
+      data.addColumn('date', 'Date');
+      data.addColumn('number', 'Data Gravity');
+      data.addColumn('number', 'Data Mass(MB)');
+      data.addColumn('number', 'Application Mass(MB)');
+      data.addColumn('number', 'Request per Seconds');
+      data.addColumn('number', 'Latency(seconds)');
+      data.addColumn('number', 'Average Request Size(MB)');
+      data.addColumn('number', 'Bandwidth(MBps)');
+      data.addRows([
 
+        ['Usc-csc-elections', new Date(2013,03,17,01,16,53), 0.0039856908062182,138.72732639,442.64,0.00029920411704865,0,37274.4,549],
+        ['Usc-csc-elections', new Date(2013,03,17,18,57,25), 974519.53811087,138.18092060,442.64,3,0,293.33333333333,676],
+        ['Usc-csc-elections', new Date(2013,03,17,19,03,44), 23337.702467234,138.18263340,442.64,0.125,0,404.66666666667,707],
+        ['Usc-csc-elections', new Date(2013,03,18,19,04,29), 8299.2679005081,138.19342899,442.64,0.12,0,642.33333333333,683],
+        ['Usc-csc-elections', new Date(2013,03,18,19,05,00), 1387.3303853396,138.22898197,442.64,0.10309278350515,0,1656.8,777],
+        ['Usc-csc-elections', new Date(2013,03,19,19,06,32), 821.33456626359,138.23799992,442.64,0.058823529411765,0,1597.2727272727,763],
+        ['Usc-csc-elections', new Date(2013,03,19,20,06,13), 28.332190609032,138.37633610,442.64,0.0066418703506908,0,2398.64,633],
+        
+        ['Get-14', new Date(2013,03,16,20,06,13), 27.528223542466,138.39472294,442.64,0.0063387423935091,0,2478.8,660],
+        ['Get-14', new Date(2013,03,16,20,06,13), 27.195559243275,138.39472294,442.64,0.0063387423935091,0,2478.8,656],
+        ['Get-14', new Date(2013,03,17,20,06,13), 26.782572694033,138.39472294,442.64,0.0063387423935091,0,2478.8,651],
+        ['Get-14', new Date(2013,03,18,20,06,13), 28.708474008212,138.39472294,442.64,0.0063387423935091,0,2478.8,674],
+        ['Get-14', new Date(2013,03,19,20,06,13), 28.538350092289,138.39472294,442.64,0.0063387423935091,0,2478.8,672],
+        ['Get-14', new Date(2013,03,19,20,06,13), 34.793500611649,138.39472294,442.64,0.0063387423935091,0,2478.8,742],
+        ['Get-14', new Date(2013,03,20,20,06,13), 31.946958432991,138.39472294,442.64,0.0063387423935091,0,2478.8,711],
+       
+      ]);
+    
+    	var motionchart = new google.visualization.MotionChart(
+          document.getElementById('visualization'));
+	    var options = {};
+		//options['state'] = '{"iconKeySettings":[],"stateVersion":3,"time":"notime","xAxisOption":"_NOTHING","playDuration":15,"iconType":"BUBBLE","sizeOption":"_NOTHING","xZoomedDataMin":null,"xZoomedIn":false,"duration":{"multiplier":1,"timeUnit":"hours"},"yZoomedDataMin":null,"xLambda":1,"colorOption":"_NOTHING","nonSelectedAlpha":0.4,"dimensions":{"iconDimensions":[]},"yZoomedIn":false,"yAxisOption":"_NOTHING","yLambda":1,"yZoomedDataMax":null,"showTrails":true,"xZoomedDataMax":null};';
+		options['width'] = 1000;
+		options['height'] = 550;
+      	motionchart.draw(data, options);
+    }
+    
 
-	
-}
+    google.setOnLoadCallback(drawVisualization);
--- views/public/index.php
+++ views/public/index.php
@@ -2,48 +2,142 @@
 <html>
 <head>
 <title>dGrav :: Data Gravity Monitoring Application</title>
+<link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+<link href="../../assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" >
+<link href='http://fonts.googleapis.com/css?family=Electrolize' rel='stylesheet' type='text/css'>
 <style>
 *	{
 		margin:0;
 		padding:0;
-		color:#000;
+		color:#fff;
 	}
 body{
-		padding:10px;
+	background-color:#111;
+	padding:10px;
 }
-.active-custom{
-	background-color:#040612;
+a{
+	color:#000;
+	-webkit-transition: color 0.2s linear;
 }
-#data-conf{
-	display:block;
+a:hover{
+	text-decoration: none;
+	color:#8293BA;
+	-webkit-transition: color 0.2s linear;
 }
-#app-settings{
-	display:none;
+#hdr{
+	height:250px;
+	margin-top:200px;
+	/*border-radius: 20px;
+	background-color:rgba(20,20,20,0.3);*/
 }
+#m_logo{
+	width:250px;
+}
+
+#menu_list{
+	margin-top:10px;
+	list-style-type: none;
+}
+#menu_list li{
+	margin-bottom:10px;
+}
+#home_cnt1{
+	background-color: #eee;
+	width:500px;
+	height:500px;
+	margin-top:60px;
+	margin-left:15%;
+	text-align: center;
+	color:#8293BA;
+	font-family: Century Gothic;
+	border:10px double #000;
+	border-radius: 300px;
+}
+.trans_item{
+	color:#CFCFCF;
+	font-family: 'Electrolize', sans-serif;
+	font-weight: bold;
+	font-size: 20px;
+	width:200px;
+	opacity: 0.3;
+	-webkit-transition: opacity 0.3s linear;
+}
+.trans_item:hover{
+	color:#CECDD4;
+	text-decoration: none;
+	opacity: 1;
+	-webkit-transition: opacity 0.3s linear;
+}
+.div_ent{
+	height:650px;
+	font-family: Iceland;
+}
+
 </style>
 
 </head>
 <body>
 
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span2">
-      <!--Sidebar content-->
-    </div>
-    <div class="span10">
-      <!--Body content-->
-    </div>
+<div class="container-fluid" id="content">
+  <div class="row-fluid" >
+  	<div id="hdr" class="span2">
+	<div id="m_logo">
+		<a href="" class="trans_item"><img src="../../assets/images/dgrav2.png" width=180/></a>
+	</div>
+
+	<ul id='menu_list'>
+		<li><a href="#monitor" class="trans_item">Monitoring</a></li>
+		<li><a href="#applist" class="trans_item">Applications</a></li>
+		<li><a href="#about" class="trans_item">About</a></li>
+		<li><a href="#contact" class="trans_item">Contacts</a></li>
+	</ul>
+
+	</div>
+	<div id="cnt" class="span10" style="height:650px;overflow-y:hidden;">
+	    <div id="home" class="div_ent">
+	    	<div id="home_cnt1">
+	    		<img src="../../assets/images/dgrav1.png" style="margin-top:20px;"/>
+				<h2>Visualizing Data Gravity</h2>
+				<br/>
+				<a href='http://blog.mccrory.me/2010/12/07/data-gravity-in-the-clouds/' target='new'><i class="icon-edit"></i>Data Gravity Defined</a>
+				<br/>
+				<br/>
+				<i class="icon-arrow-down"></i>
+				<br/>
+				<br/>
+				<a href='http://datagravity.org/' target='new'><i class="icon-cog"></i>Data Gravity Formula</a>
+				<br/>
+				<br/>
+				<i class="icon-arrow-down"></i>
+				<br/>
+				<br/>
+				<a href='https://www.facebook.com/pprmint' target='new'><i class="icon-leaf"></i>pprmint</a>
+
+	    	</div>
+	    	<div style="float:right;">
+	    		<p>
+	    			You can't blame gravity for falling in love.
+	    			<br/>-Albert Einstein
+	    		</p>
+	    	</div>
+	    </div>
+	   
+	    <div id="monitor" class="div_ent">
+	    	<h2>Monitor</h2>
+	    	<div id="visualization">
+	    	</div>
+		</div>
+  	</div>
   </div>
 </div>
 
 
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
 <script src="../../assets/js/jquery.min.js"></script>
-<link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
-<link href="../../assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" >
 <script src="../../assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../assets/js/dygraph-combined.js"></script>
 <script src="../../assets/js/public.js"></script>
 <script type='text/javascript'>
-	
 </script>
 
 </body>
